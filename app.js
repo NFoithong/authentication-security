9a16edd25083c709032a74a503d8bf7cda2fa35f
NFoithong <n.foithong1983@gmail.com>
2022-03-30 23:29:37 -0400

    register with username and password

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/secrests', { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model('User', userSchema);
 
 // home
 app.get('/', (req, res) => {
@@ -24,6 +35,39 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
+// register post route 
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+// login post route
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
 
 
 app.listen(3000, function() {
